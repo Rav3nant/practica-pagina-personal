5b6dd71bbef9913380f70dac8a2af3879513751e
Iván Pineda <34119631+Rav3nant@users.noreply.github.com>
2023-10-14 17:06:41 -0700

    Add files via upload

---
+++ src/components/CardFooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
+        <div class="text-center my-4 dark:text-white flex items-center justify-center">
+            
+        </div>
+        <div class="text-center">
+            
+            <a href="https://www.facebook.com/jesusivan.pinedaramos/" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-facebook" /></a>
+            <a href="https://www.instagram.com/god_of_corn_bread/" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-instagram" /></a>
+            <a href="https://github.com/Rav3nant" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-brands fa-github" /></a>
+            <a href="mailto:pineda.ramos@hotmail.com"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-solid fa-envelope" /></a>
+        </div>
+    </div>
+</template>
+<script setup></script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Iván Pineda, reportandose </h2>
+        <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
+            Estudiante de Sistemas con un pie en la salida. Disponible para trabajar en proyectos simples,
+             y me rento para fiestas y piñatas si la paga es buena</p>
+        
+    </div>
+</template>
+<script setup>
+
+
+</script>
